6c96952376337db2f37b3cc2ea5eb1ba13fb6548
Gaurav Adhikari <gauravadhikari@Gauravs-Air.lan>
2023-04-20 13:46:23 -0500

    feat(appointment-models) added patient, appointment, doctor and clinic models

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Appointments, Clinic,Doctor, Patient
 # Register your models here.
+
+admin.site.register(Appointments)
+admin.site.register(Clinic)
+admin.site.register(Doctor)
+admin.site.register(Patient)
