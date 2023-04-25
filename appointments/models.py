a46658f784284ff6b7adc7c4c37dfefe58678a60
Gaurav Adhikari <gauravadhikari@Gauravs-Air.lan>
2023-04-25 01:05:54 -0500

    feat(appointments-migrations) added appointment migrations from AMR

--- appointments/models.py
+++ appointments/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from . import constants as c
+
+
 class Patient(models.Model):
     class Meta:
         db_table = "PATIENT"
+
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    )
 
     id = models.BigAutoField(db_column="ID", primary_key=True)
 
@@ -10,15 +18,15 @@ class Patient(models.Model):
 
     modified_date = models.DateTimeField(db_column="MODIFIED_DATE", auto_now=True)
 
-    first_name =  models.CharField(
+    first_name = models.CharField(
         db_column="FIRST_NAME", max_length=300, null=False, blank=False
     )
 
-    middle_name =  models.CharField(
-        db_column="MIDDLE_NAME", max_length=300, null=False, blank=False
+    middle_name = models.CharField(
+        db_column="MIDDLE_NAME", max_length=300, null=True, blank=False
     )
 
-    last_name =  models.CharField(
+    last_name = models.CharField(
         db_column="LAST_NAME", max_length=300, null=False, blank=False
     )
 
@@ -27,14 +35,10 @@ class Patient(models.Model):
     street = models.CharField(
         db_column="STREET", null=False, blank=False, max_length=300
     )
-    
-    city = models.CharField(
-        db_column="CITY", null=False, blank=False, max_length=300
-    )
 
-    state = models.CharField(
-        db_column="STATE", null=False, blank=False, max_length=300
-    )
+    city = models.CharField(db_column="CITY", null=False, blank=False, max_length=300)
+
+    state = models.CharField(db_column="STATE", null=False, blank=False, max_length=300)
 
     zipcode = models.CharField(
         db_column="ZIPCODE", null=False, blank=False, max_length=7
@@ -46,13 +50,23 @@ class Patient(models.Model):
         db_column="DATE_OF_BIRTH", null=False, blank=False, max_length=8
     )
 
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
     @property
     def full_name(self):
-        return f"{self.first_name} {self.middle_name} {self.last_name}"
-    
+        return (
+            f"{self.first_name} {self.middle_name} {self.last_name}"
+            if self.middle_name is not None
+            else f"{self.first_name} {self.last_name}"
+        )
+
     @property
     def full_address(self):
         return f"{self.street} {self.city} {self.state} {self.zipcode}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Clinic(models.Model):
     class Meta:
@@ -64,22 +78,17 @@ class Clinic(models.Model):
 
     modified_date = models.DateTimeField(db_column="MODIFIED_DATE", auto_now=True)
 
-
-    clinic_name =  models.CharField(
+    clinic_name = models.CharField(
         db_column="CLINIC_NAME", max_length=300, null=False, blank=False
     )
 
     street = models.CharField(
         db_column="STREET", null=False, blank=False, max_length=300
     )
-    
-    city = models.CharField(
-        db_column="CITY", null=False, blank=False, max_length=300
-    )
 
-    state = models.CharField(
-        db_column="STATE", null=False, blank=False, max_length=300
-    )
+    city = models.CharField(db_column="CITY", null=False, blank=False, max_length=300)
+
+    state = models.CharField(db_column="STATE", null=False, blank=False, max_length=300)
 
     zipcode = models.CharField(
         db_column="ZIPCODE", null=False, blank=False, max_length=7
@@ -88,18 +97,20 @@ class Clinic(models.Model):
     phone = models.CharField(db_column="PHONE", null=False, blank=False, max_length=20)
 
     @property
-    def full_name(self):
+    def clinic_name(self):
         return f"{self.clinic_name}"
-    
+
     @property
     def full_address(self):
         return f"{self.street} {self.city} {self.state} {self.zipcode}"
+
+    def __str__(self):
+        return f"{self.clinic_name}"
 
 
 class Doctor(models.Model):
     class Meta:
         db_table = "DOCTOR"
-
 
     SPECIALIZATION_CHOICES = (
         (c.PHISIOTHERAPY, c.PHISIOTHERAPY),
@@ -122,15 +133,15 @@ class Doctor(models.Model):
 
     modified_date = models.DateTimeField(db_column="MODIFIED_DATE", auto_now=True)
 
-    first_name =  models.CharField(
+    first_name = models.CharField(
         db_column="FIRST_NAME", max_length=300, null=False, blank=False
     )
 
-    middle_name =  models.CharField(
-        db_column="MIDDLE_NAME", max_length=300, null=False, blank=False
+    middle_name = models.CharField(
+        db_column="MIDDLE_NAME", max_length=300, null=True, blank=False
     )
 
-    last_name =  models.CharField(
+    last_name = models.CharField(
         db_column="LAST_NAME", max_length=300, null=False, blank=False
     )
 
@@ -139,24 +150,16 @@ class Doctor(models.Model):
     street = models.CharField(
         db_column="STREET", null=False, blank=False, max_length=300
     )
-    
-    city = models.CharField(
-        db_column="CITY", null=False, blank=False, max_length=300
-    )
 
-    state = models.CharField(
-        db_column="STATE", null=False, blank=False, max_length=300
-    )
+    city = models.CharField(db_column="CITY", null=False, blank=False, max_length=300)
+
+    state = models.CharField(db_column="STATE", null=False, blank=False, max_length=300)
 
     zipcode = models.CharField(
         db_column="ZIPCODE", null=False, blank=False, max_length=7
     )
 
     phone = models.CharField(db_column="PHONE", null=False, blank=False, max_length=20)
-
-    date_of_birth = models.DateField(
-        db_column="DATE_OF_BIRTH", null=False, blank=False, max_length=8
-    )
 
     specialization = models.CharField(
         db_column="SPECIALIZATION",
@@ -176,7 +179,7 @@ class Doctor(models.Model):
     @property
     def full_name(self):
         return f"{self.first_name} {self.middle_name} {self.last_name}"
-    
+
     @property
     def full_address(self):
         return f"{self.street} {self.city} {self.state} {self.zipcode}"
@@ -186,13 +189,11 @@ class Appointments(models.Model):
     class Meta:
         db_table = "APPOINTMENTS"
 
-
     id = models.BigAutoField(db_column="ID", primary_key=True)
 
     created_date = models.DateTimeField(db_column="CREATED_DATE", auto_now_add=True)
 
     modified_date = models.DateTimeField(db_column="MODIFIED_DATE", auto_now=True)
-
 
     clinic = models.ForeignKey(
         Clinic,
@@ -213,14 +214,5 @@ class Appointments(models.Model):
     )
 
     appointment_datetime = models.DateTimeField(
-        db_column="APPOINTMENT_DATETIME",
-        blank=True,
-        null=True,)
-
-
-
-
-
-
-
-
+        db_column="APPOINTMENT_DATETIME", blank=True, null=True, default=None
+    )
