0823bc75a4d646b0b7d50d9880f0a7c0caa285d3
Gaurav Adhikari <gauravadhikari@Gauravs-Air.lan>
2023-04-27 15:27:27 -0500

    feat(caretrack-doc) added caretrack swagger docs

--- caretrack/urls.py
+++ caretrack/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+
+urlpatterns = [
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appointments.urls')),
+    path("docs/", include_docs_urls(title="CareTrack API")),
+
+
 ]
